b8c5633157e8046bc63a4b6202a1b4fb056a7dfc
Carlos  chen <ccjjww1222@gmail.com>
2022-09-23 10:15:39 +0800

    Update go.py

--- common/go.py
+++ common/go.py
@@ -59,6 +59,7 @@ class go ():
                 # print(first_data.get("centery"),second_data.get("centery"),second_data.get("centery")-first_data.get("centery"))
                 if (second_data.get("centery")-first_data.get("centery") < 10):
                     self.current_machine_speed += self.increment
+                    self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=150 else 150
                     self.send_comand("MF "+str(self.current_machine_speed))
                 else:
                     self.current_machine_speed = self.default_machine_speed
