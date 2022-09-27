87708136a95c1c88d5b31166c9c8ae1ddac9a91f
Carlos  chen <ccjjww1222@gmail.com>
2022-09-27 17:38:46 +0800

    1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+sudo chmod -R 777 /dev/ttyACM0
+
 python3 service.py 
 
 python3 track.py --source 0 --yolo-weights ./weights/best.pt --camera_device 0
--- common/go.py
+++ common/go.py
@@ -85,8 +85,8 @@ class go ():
                             self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=25 else 25
                             self.send_comand("MF "+str(self.current_machine_speed))
                         else:
-                            self.current_machine_speed = self.default_machine_speed
-                            self.send_comand("MF "+str(self.default_machine_speed))
+                            # self.current_machine_speed = self.default_machine_speed
+                            self.send_comand("MF "+str(self.current_machine_speed))
                     else:
                         print("1秒内，不重复加速")
     
