6e1292aa937de502e1ff7da33c98f1fc1f9bcbe5
Carlos  chen <ccjjww1222@gmail.com>
2022-09-27 09:24:24 +0800

    Update track.py

--- track.py
+++ track.py
@@ -356,7 +356,7 @@ def add_points(item_points,flag):
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-weights', nargs='+', type=Path, default=WEIGHTS / 'yolov5m.pt', help='model.pt path(s)')
+    parser.add_argument('--yolo-weights', nargs='+', type=Path, default='./weights/best.pt', help='model.pt path(s)')
     parser.add_argument('--strong-sort-weights', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt')
     parser.add_argument('--config-strongsort', type=str, default='strong_sort/configs/strong_sort.yaml')
     parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')  
