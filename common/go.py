b7d19ed75cadf9a4843ca36684d263c969d2d8c8
Carlos  chen <ccjjww1222@gmail.com>
2022-09-27 15:53:10 +0800

    Update go.py

--- common/go.py
+++ common/go.py
@@ -47,47 +47,48 @@ class go ():
     def is_add_speed(self, redis_key):
         # print("redis_key",redis_key)
         data = self.redis.get(redis_key)
-        if (data and len(data)>=2):
+        if (data ):
             data = json.loads(data)
-            first = data[0]
-            second = data[1]
-            index_list = [-1, -1]
-            flag = False
-            for i in range(len(first)):
-                index = first[i]["track_id"]
-                for j in range(len(second)):
-                    index2 = second[j]["track_id"]
-                    if (index == index2):
-                        flag = True
-                        index_list[1] = j
+            if(len(data)>=2):
+                first = data[0]
+                second = data[1]
+                index_list = [-1, -1]
+                flag = False
+                for i in range(len(first)):
+                    index = first[i]["track_id"]
+                    for j in range(len(second)):
+                        index2 = second[j]["track_id"]
+                        if (index == index2):
+                            flag = True
+                            index_list[1] = j
+                            break
+                    if (flag):
+                        index_list[0] = i
                         break
-                if (flag):
-                    index_list[0] = i
-                    break
-            if (not index_list[0] == -1) and (not index_list[1] == -1):
-                first_data = first[index_list[0]]
-                second_data = second[index_list[1]]
-                time1= first_data.get("time")
-                time2= second_data.get("time")
-                difftime  = time2-time1
-                diff_px = abs(second_data.get("centery")-first_data.get("centery"))
-                diff_time = abs(second_data.get("time")-first_data.get("time"))
-                now = float(time.time())
-                last_diff_time  = now - self.last_check_time
-                print("-------------------------------------------------------------------------------------------------------")
-                print("last_diff_time:{},difftime:{},diff_px:{},diff_px/diff_time:{}".format(last_diff_time,difftime,diff_px,diff_px/diff_time))
-                print("-------------------------------------------------------------------------------------------------------")
-                if (float(time.time()) - self.last_check_time >1) :
-                    self.last_check_time  = now 
-                    if (abs(diff_px/diff_time) < self.min_unit_px):
-                        self.current_machine_speed += self.increment
-                        self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=25 else 25
-                        self.send_comand("MF "+str(self.current_machine_speed))
+                if (not index_list[0] == -1) and (not index_list[1] == -1):
+                    first_data = first[index_list[0]]
+                    second_data = second[index_list[1]]
+                    time1= first_data.get("time")
+                    time2= second_data.get("time")
+                    difftime  = time2-time1
+                    diff_px = abs(second_data.get("centery")-first_data.get("centery"))
+                    diff_time = abs(second_data.get("time")-first_data.get("time"))
+                    now = float(time.time())
+                    last_diff_time  = now - self.last_check_time
+                    print("-------------------------------------------------------------------------------------------------------")
+                    print("last_diff_time:{},difftime:{},diff_px:{},diff_px/diff_time:{}".format(last_diff_time,difftime,diff_px,diff_px/diff_time))
+                    print("-------------------------------------------------------------------------------------------------------")
+                    if (float(time.time()) - self.last_check_time >1) :
+                        self.last_check_time  = now 
+                        if (abs(diff_px/diff_time) < self.min_unit_px):
+                            self.current_machine_speed += self.increment
+                            self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=25 else 25
+                            self.send_comand("MF "+str(self.current_machine_speed))
+                        else:
+                            self.current_machine_speed = self.default_machine_speed
+                            self.send_comand("MF "+str(self.default_machine_speed))
                     else:
-                        self.current_machine_speed = self.default_machine_speed
-                        self.send_comand("MF "+str(self.default_machine_speed))
-                else:
-                    print("1秒内，不重复加速")
+                        print("1秒内，不重复加速")
     
     def get_target_x (self,frame):
         total = 0
