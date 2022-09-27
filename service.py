517f350d7a60754eb303d53be9971a3012cc1b40
Carlos  chen <ccjjww1222@gmail.com>
2022-09-27 17:55:26 +0800

    Update service.py

--- service.py
+++ service.py
@@ -1,4 +1,11 @@
 from common.unix_socket import unix_socket
 
 s = unix_socket()
-s.server()
+
+
+
+try:
+    s.server()
+except KeyboardInterrupt:
+    print("ctrl+c stop")
+    s.send_message("STOP 0")
