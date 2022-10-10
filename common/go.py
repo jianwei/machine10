edff67efe1910acc59b4fc5a67fa406b39177f8e
Carlos  chen <ccjjww1222@gmail.com>
2022-10-10 14:52:29 +0800

    Update go.py

--- common/go.py
+++ common/go.py
@@ -82,12 +82,18 @@ class go ():
                     if (float(time.time()) - self.last_check_time >0.2) :
                         self.last_check_time  = now 
                         if (abs(diff_px/diff_time) < self.min_unit_px):
-                            self.current_machine_speed += self.increment
-                            self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=20 else 20
-                            self.send_comand("MF "+str(self.current_machine_speed))
-                        else:
-                            # self.current_machine_speed = self.default_machine_speed
-                            self.send_comand("MF "+str(self.current_machine_speed))
+                            new_speed = self.current_machine_speed
+                            new_speed += self.increment
+                            new_speed = new_speed if  new_speed <=20 else 20
+                            if(new_speed!=self.current_machine_speed):
+                                self.current_machine_speed = new_speed
+                                self.send_comand("MF "+str(self.current_machine_speed))
+                            # self.current_machine_speed += self.increment
+                            # self.current_machine_speed = self.current_machine_speed if  self.current_machine_speed <=20 else 20
+                            # self.send_comand("MF "+str(self.current_machine_speed))
+                        # else:
+                        #     # self.current_machine_speed = self.default_machine_speed
+                        #     self.send_comand("MF "+str(self.current_machine_speed))
                     else:
                         print("0.2秒内，不重复加速")
     
